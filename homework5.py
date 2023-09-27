c8d929b07c54e3c12b42de246be1a9e032a6c46b
weichen11011 <99935264+weichen11011@users.noreply.github.com>
2023-09-27 11:28:05 +0800

    Create homework5.py

---
+++ homework5.py
@@ -0,0 +1,9 @@
+f1 = lambda x: (x**2 + 1 )/3
+f2 = lambda x: -((x-2)**2 -3)
+f3 = lambda x: ((x-1)*(x+1)+2)/3
+
+x1 = x2 = x3 = 1
+
+for i in range(20):
+    x1, x2, x3 = f1(x1), f2(x2), f3(x3)
+    print('x1:', x1, 'x2', x2, 'x3', x3)
